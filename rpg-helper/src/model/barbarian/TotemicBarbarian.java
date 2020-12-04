460a0e29bfa95265e55fd1aced4ac30072cd892a
kolepcruz <kikeponce.cruz@gmail.com>
2020-12-03 23:13:48 -0300

    Adiciona metodos TotemicBarbarian

--- rpg-helper/src/model/barbarian/TotemicBarbarian.java
+++ rpg-helper/src/model/barbarian/TotemicBarbarian.java
@@ -3,6 +3,7 @@ package model.barbarian;
 import model.adv_tier.Atributos;
 import model.adv_tier.Combate;
 import model.adv_tier.Inventory;
+import utils.RandomUtil;
 
 public class TotemicBarbarian extends Barbarian{
 
@@ -11,5 +12,34 @@ public class TotemicBarbarian extends Barbarian{
     }
 
     public TotemicBarbarian() {
+    }
+
+    public int spiritTotem()
+    {
+        if(this.getLvl() == 2)
+        {
+            int damage = 0;
+            for (int i = 0;i<4;i++)
+            {
+                damage += RandomUtil.getRandom(6);
+            }
+            this.setRage(this.getRage() - 10);
+            return damage;
+        }
+        return 0;
+    }
+    public int totemicHarmony()
+    {
+        if(this.getLvl() == 3)
+        {
+            int damage = 0;
+            for (int i = 0; i < 5;++i)
+            {
+                damage += RandomUtil.getRandom(10);
+            }
+            this.setRage(this.getRage() - 20);
+            return damage;
+        }
+        return 0;
     }
 }
