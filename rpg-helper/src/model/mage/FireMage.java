51b1b68049e2785c735ab7599679df898b8411e9
kolepcruz <kikeponce.cruz@gmail.com>
2020-12-06 15:59:08 -0300

    Adiciona FrostMage e FireMage

--- rpg-helper/src/model/mage/FireMage.java
+++ rpg-helper/src/model/mage/FireMage.java
@@ -3,6 +3,7 @@ package model.mage;
 import model.adv_tier.Atributos;
 import model.adv_tier.Combate;
 import model.adv_tier.Inventory;
+import utils.RandomUtil;
 
 public class FireMage extends Mage {
 
@@ -11,5 +12,65 @@ public class FireMage extends Mage {
     }
 
     public FireMage() {
+    }
+
+    public int metheorStorm()
+    {
+        if(this.getLvl() >= 1)
+        {
+            int damage = 0;
+            for (int i = 0; i<3;++i)
+            {
+                damage+= RandomUtil.getRandom(6);
+            }
+            this.setMana(this.getMana()-10);
+            return damage;
+        }
+        return 0;
+    }
+
+    public int fireball()
+    {
+        if(this.getLvl() >= 2)
+        {
+            int damage = 0;
+            for (int i = 0; i<4;++i)
+            {
+                damage+= RandomUtil.getRandom(10);
+            }
+            this.setMana(this.getMana()-15);
+            return damage;
+        }
+        return 0;
+    }
+
+    public int burningBeam()
+    {
+        if(this.getLvl() >= 3)
+        {
+            int damage = 0;
+            for (int i = 0; i<5;++i)
+            {
+                damage+= RandomUtil.getRandom(10);
+            }
+            this.setMana(this.getMana()-20);
+            return damage;
+        }
+        return 0;
+    }
+
+    public int flamingSphere()
+    {
+        if(this.getLvl() >= 4)
+        {
+            int damage = 0;
+            for (int i = 0; i<8;++i)
+            {
+                damage+= RandomUtil.getRandom(6);
+            }
+            this.setMana(this.getMana()-3);
+            return damage;
+        }
+        return 0;
     }
 }
--- rpg-helper/src/model/mage/FrostMage.java
+++ rpg-helper/src/model/mage/FrostMage.java
@@ -3,6 +3,7 @@ package model.mage;
 import model.adv_tier.Atributos;
 import model.adv_tier.Combate;
 import model.adv_tier.Inventory;
+import utils.RandomUtil;
 
 public class FrostMage extends Mage{
 
@@ -11,5 +12,65 @@ public class FrostMage extends Mage{
     }
 
     public FrostMage() {
+    }
+
+    public int iceBeam()
+    {
+        if(this.getLvl() >= 1)
+        {
+            int damage = 0;
+            for (int i = 0; i<3;++i)
+            {
+                damage+= RandomUtil.getRandom(4);
+            }
+            this.setMana(this.getMana()-5);
+            return damage;
+        }
+        return 0;
+    }
+
+    public int iceStorm()
+    {
+        if(this.getLvl() >= 2)
+        {
+            int damage = 0;
+            for (int i = 0; i<4;++i)
+            {
+                damage+= RandomUtil.getRandom(6);
+            }
+            this.setMana(this.getMana()-10);
+            return damage;
+        }
+        return 0;
+    }
+
+    public int otilukeFreezingSphere()
+    {
+        if(this.getLvl() >= 3)
+        {
+            int damage = 0;
+            for (int i = 0; i<5;++i)
+            {
+                damage+= RandomUtil.getRandom(10);
+            }
+            this.setMana(this.getMana()-20);
+            return damage;
+        }
+        return 0;
+    }
+
+    public int frostIcicle()
+    {
+        if(this.getLvl() >= 4)
+        {
+            int damage = 0;
+            for (int i = 0; i<8;++i)
+            {
+                damage+= RandomUtil.getRandom(10);
+            }
+            this.setMana(this.getMana()-35);
+            return damage;
+        }
+        return 0;
     }
 }
