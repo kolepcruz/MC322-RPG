ab23551fc5726475978760aed5ff000901a1888e
Tr0fino <guitrofino@gmail.com>
2020-12-11 19:18:37 -0300

    Renoado Método

--- rpg-helper/src/model/mage/FrostMage.java
+++ rpg-helper/src/model/mage/FrostMage.java
@@ -29,7 +29,7 @@ public class FrostMage extends Mage{
         return doDamage(3,5,10,20);
     }
 
-    public int frostIcicle()
+    public int frostCicle()
     {
         return doDamage(4,8,10,35);
     }
