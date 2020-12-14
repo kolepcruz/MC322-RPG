45a20a2e8d66f8e6f101b1fd1b0a2e02a0ee8658
Tr0fino <guitrofino@gmail.com>
2020-12-14 14:29:50 -0300

    Revisão Barbarian e filhas

--- rpg-helper/src/model/barbarian/Barbarian.java
+++ rpg-helper/src/model/barbarian/Barbarian.java
@@ -1,11 +1,8 @@
 package model.barbarian;
 
 import enums.*;
-import model.Person;
 import model.adv_tier.Adventurer;
 import model.adv_tier.Attributes;
-import model.adv_tier.CombatAttributes;
-import model.adv_tier.Inventory;
 import utils.RandomUtil;
 
 public class Barbarian extends Adventurer {
