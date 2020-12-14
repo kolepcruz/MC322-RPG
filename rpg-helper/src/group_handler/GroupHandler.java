e19909e29b0bb638f3f91cc3602f41a13a8fbff9
Tr0fino <guitrofino@gmail.com>
2020-12-14 14:04:21 -0300

    Revisão GruopHandler

--- rpg-helper/src/group_handler/GroupHandler.java
+++ rpg-helper/src/group_handler/GroupHandler.java
@@ -1,12 +1,10 @@
 package group_handler;
 
 import model.adv_tier.Adventurer;
-
 import java.util.ArrayList;
 
 /**
- *  this class is planned to be used as the main event handling class when something affects the entire party
- *
+ * this class is planned to be used as the main event handling class when something affects the entire party
  * @author kole
  */
 
@@ -18,7 +16,6 @@ public class GroupHandler {
     public GroupHandler(String narratorName) {
         this.narratorName = narratorName;
     }
-
     public ArrayList<Adventurer> getGrupo() {
         return grupo;
     }
