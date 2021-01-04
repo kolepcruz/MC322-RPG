0463f8bd6be4ab5dca1248728527de94e9ed0dd7
Tr0fino <guitrofino@gmail.com>
2021-01-04 18:53:08 -0300

    testes de interação com o usuário

--- rpg-helper/src/utils/GraphicInterface.java
+++ rpg-helper/src/utils/GraphicInterface.java
@@ -10,72 +10,76 @@ public class GraphicInterface extends JFrame implements ActionListener
 {
     private static final long serialVersionUID = -2122161377842820073L;
 
-    public GraphicInterface()
+    JButton startButton;
+
+    GraphicInterface()
     {
         super("RPG Helper"); //frame name
         this.setSize(1080,720); //size of frame
         this.setLayout(null); //no layout
-        
+        this.getContentPane().setBackground(new Color(96, 85, 91)); //set color
+
         Dimension dim = Toolkit.getDefaultToolkit().getScreenSize(); //screen size
         this.setLocation((dim.width-this.getWidth())/2, (dim.height-this.getHeight())/2); //center window
 
 
-        Container container = getContentPane();  //get container
-        container.setLayout(null); //set layout
-        container.setBackground(new Color(96, 85, 91)); //set color
+        // Container container = getContentPane();  //get container
+        // container.setLayout(null); //set layout
+        // container.setBackground(new Color(96, 85, 91)); //set color
 
-        JPanel panel = new JPanel();
-        panel.setBounds(50, 50, 960, 479); //position and size
-        panel.setLayout(null);
-        container.add(panel);
+        // JPanel panel = new JPanel();
+        // panel.setBounds(50, 50, 960, 479); //position and size
+        // panel.setLayout(null);
+        // container.add(panel);
 
         Image image = new ImageIcon("RPG Git.png").getImage(); //get image
         image = image.getScaledInstance(960, 479, java.awt.Image.SCALE_AREA_AVERAGING); //transform it
 
-        JLabel picture = new JLabel("");
-        picture.setIcon(new ImageIcon(image)); //load image
-        picture.setBorder(BorderFactory.createLineBorder(Color.BLACK)); //set board with color
-        picture.setSize(960, 479); //width height
-        panel.add(picture);
+        JLabel projectPicture = new JLabel("");
+        projectPicture.setIcon(new ImageIcon(image)); //load image
+        projectPicture.setBorder(BorderFactory.createLineBorder(Color.BLACK)); //set board with color
+        projectPicture.setBounds(50, 50, 960, 479); //width height
+        this.getContentPane().getParent().add(projectPicture);
 
         JLabel projectName = new JLabel("RPG HELPER");
         projectName.setFont(new Font("Impact", Font.BOLD, 60));
         projectName.setForeground(Color.BLACK);
         projectName.setBounds(50, 575, 500, 50);
-        container.add(projectName);
+        this.add(projectName);
 
 
-        JButton start = new JButton("Start");
-        start.setFont(new Font("Impact", Font.BOLD, 20));
-        start.setForeground(Color.BLACK);
-        start.setBounds(910, 575, 100, 40);
-        this.getContentPane().add(start);
-
-        start.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                start.setText("Done");
-                container.remove(picture);
-            }
-        });
+        startButton = new JButton("Start");
+        startButton.setFont(new Font("Impact", Font.BOLD, 20));
+        startButton.setForeground(Color.BLACK);
+        startButton.setBounds(910, 575, 100, 40);
+        startButton.addActionListener(this);
+        this.getContentPane().add(startButton);
 
         JTextField textField = new JTextField();
         textField.setPreferredSize(new Dimension(500, 50));
 
-        container.add(textField);
+        this.add(textField);
 
         setDefaultCloseOperation(EXIT_ON_CLOSE); //close operation
         setVisible(true); //make visible
     }
 
-    public static void main(String[] args) {
-        GraphicInterface myGraphicInterface = new GraphicInterface();
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource() == startButton)
+        {
+            startButton.setText("Done");
+            System.out.println("Botão Prescionado");
+        }
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+
+    public static void main(String[] args)
+    {
+        GraphicInterface myFrame = new GraphicInterface();
+        System.out.println(myFrame.startButton.getText());
+
 
     }
 }
